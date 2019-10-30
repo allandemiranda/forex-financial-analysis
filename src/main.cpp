28a6fe69fa443dd039fb01464f236efbecec0f65
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-10-30 19:28:12 -0300

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -228,7 +228,7 @@ void ShotHammer(std::string arquivo, std::string tempo) {
   for (unsigned long i = 6; i < grafico.chart.size() - 3; ++i) {
     if (!*grafico.chart.at(i).getStatus() or
         !*grafico.chart.at(i - 1).getStatus() or
-        !*grafico.chart.at(i - 5).getStatus() or
+        !*grafico.chart.at(i - 2).getStatus() or
         !*grafico.chart.at(i + 1).getStatus() or
         !*grafico.chart.at(i + 2).getStatus()) {
       continue;
@@ -236,7 +236,8 @@ void ShotHammer(std::string arquivo, std::string tempo) {
     if (*grafico.chart.at(i).getName() == "WHITE HAMMER") {
       if (*grafico.chart.at(i - 1).getType() == 1) {
         if (*grafico.chart.at(i - 1).getClose() <
-            *grafico.chart.at(i - 5).getClose()) {
+            *grafico.chart.at(i - 2).getClose()) {
+              //
           if (*grafico.chart.at(i - 1).getClose() >=
               *grafico.chart.at(i).getClose()) {
             if (*grafico.chart.at(i + 1).getType() == 0) {
@@ -263,7 +264,8 @@ void ShotHammer(std::string arquivo, std::string tempo) {
         }
       } else {
         if (*grafico.chart.at(i - 1).getClose() >
-            *grafico.chart.at(i - 5).getClose()) {
+            *grafico.chart.at(i - 2).getClose()) {
+          //
           if (*grafico.chart.at(i - 1).getClose() <=
               *grafico.chart.at(i).getOpen()) {
             if (*grafico.chart.at(i + 1).getType() == 1) {
@@ -293,7 +295,8 @@ void ShotHammer(std::string arquivo, std::string tempo) {
       if (*grafico.chart.at(i).getName() == "BLACK HAMMER") {
         if (*grafico.chart.at(i - 1).getType() == 1) {
           if (*grafico.chart.at(i - 1).getClose() <
-              *grafico.chart.at(i - 5).getClose()) {
+              *grafico.chart.at(i - 2).getClose()) {
+                //
             if (*grafico.chart.at(i - 1).getClose() >=
                 *grafico.chart.at(i).getOpen()) {
               if (*grafico.chart.at(i + 1).getType() == 0) {
@@ -320,7 +323,8 @@ void ShotHammer(std::string arquivo, std::string tempo) {
           }
         } else {
           if (*grafico.chart.at(i - 1).getClose() >
-              *grafico.chart.at(i - 5).getClose()) {
+              *grafico.chart.at(i - 2).getClose()) {
+                //
             if (*grafico.chart.at(i - 1).getClose() <=
                 *grafico.chart.at(i).getClose()) {
               if (*grafico.chart.at(i + 1).getType() == 1) {
